89780bd5df9e4ab81b24f8823a2d4e9bbf83c152
Davison Alves <53572219+davisonalves@users.noreply.github.com>
2021-06-19 22:36:43 -0400

    update todo specs

--- cypress/integration/todo.spec.js
+++ cypress/integration/todo.spec.js
@@ -1,6 +1,6 @@
 describe('Todo mvc', () => {
     beforeEach('Logando no site',() => {
-        cy.visit('https://todomvc.com/examples/typescript-react/#/');
+        cy.visit('https://todomvc.com/examples/typescript-react/#/')
     });
     it('Cadastrar itens na lista', () => {
         cy.get('ul.todo-list').should('have.length', 0)
@@ -8,13 +8,13 @@ describe('Todo mvc', () => {
         cy.get('ul.todo-list').should('have.length', 1)
     });
     it('Excluir itens na lista', () => {
-        cy.get('ul.todo-list').should('have.length', 0)
+        cy.get('ul.todo-list').should('not.exist')
         cy.get('header input').type('Limpar casa {enter}')
         cy.get('button.destroy').invoke('show').click()
-        cy.get('ul.todo-list').should('have.length', 0)
+        cy.get('ul.todo-list').should('not.exist')
     });
     it('Marcar um item como completo', () => {
-        cy.get('ul.todo-list').should('have.length', 0)
+        cy.get('ul.todo-list').should('not.exist')
         cy.get('header input').type('Limpar casa {enter}')
         cy.get('ul.todo-list').should('have.length', 1)
         cy.get('input.toggle').click()
@@ -22,14 +22,27 @@ describe('Todo mvc', () => {
         cy.get('button.clear-completed').should('contain', 'Clear completed')
     });
     it('Selecionar o filtro active', () => {
-        cy.get('ul.todo-list').should('have.length', 0)
+        cy.get('ul.todo-list').should('not.exist')
         cy.get('header input').type('Limpar casa {enter}')
         cy.get('ul.todo-list').should('have.length', 1)
         cy.get('.filters').contains('Active').click()
         cy.get('input.toggle').click()
-        
-        /*
-        cy.get('strong').should('contain',0)
-        cy.get('button.clear-completed').should('contain', 'Clear completed')*/
+        cy.get('ul.todo-list').children().should('have.length', 0)
+    });
+    it('Selecionar o filtro completed', () => {
+        cy.get('ul.todo-list').should('not.exist')
+        cy.get('header input').type('Limpar casa {enter}')
+        cy.get('ul.todo-list').should('have.length', 1)
+        cy.get('input.toggle').click()
+        cy.get('.filters').contains('Completed').click()
+        cy.get('ul.todo-list').children().should('have.length', 1)
+    });
+    it('Selecionar clear completed com filtro all ativo', () => {
+        cy.get('ul.todo-list').should('not.exist')
+        cy.get('header input').type('Limpar casa {enter}')
+        cy.get('ul.todo-list').should('have.length', 1)
+        cy.get('input.toggle').click()
+        cy.get('button.clear-completed').click()
+        cy.get('ul.todo-list').should('not.exist')
     });
 });
